aada190a2ebef8eabc212f02665e6c0c5a752834
Atul Saini <atulsaini9886@gmail.com>
2021-09-04 23:49:56 +0530

    Almost done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,69 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
+import Header from "./Header";
+import Figure from "./Figure";
+import WrongLetters from "./WrongLetters";
+import Word from "./Word";
+import Popup from "./Popup";
+import Notification from "./Notification";
+import { showNotification as helpShow } from "../Helpers/helpers";
+
+const words = [
+  "applicable",
+  "programming",
+  "interface",
+  "wizard",
+  "geek",
+  "hangman",
+  "developer",
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
-  return <div></div>;
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleInput = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((correctLetters) => [...correctLetters, letter]);
+          } else {
+            helpShow(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            helpShow(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleInput);
+
+    return () => window.removeEventListener("keydown", handleInput);
+  }, [correctLetters, wrongLetters, playable]);
+
+  return (
+    <div>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup />
+      <Notification showNotification={showNotification} />
+    </div>
+  );
 }
 
 export default App;
